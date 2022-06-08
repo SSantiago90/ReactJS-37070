8550273bdf5d11fd15e3e96ce565b56a6dceb7cb
ssantiago90 <salkin.santiago@gmail.com>
2022-06-07 21:36:13 -0300

    promises examples

---
+++ src/promises/04_AsyncAwait.js
@@ -0,0 +1,25 @@
+function APIcall() {
+  return new Promise((resolve, reject) => {
+    const data = {
+      id: "1",
+      name: "Mi producto",
+      description: "El mejor producto de la tienda",
+      stock: 9,
+    };
+    setTimeout(() => {
+      console.log("resolviendo...");
+      resolve(data);
+    }, 1500);
+  });
+}
+
+console.log("App iniciada!");
+
+async function main() {
+  let resAPI = await APIcall();
+  console.log(`Respuesta: ${resAPI}`);
+}
+
+main();
+
+console.log("App finalizada.");
