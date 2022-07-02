82c15d7c00bfb6964552fd846cf30c1a3e500035
ssantiago90 <salkin.santiago@gmail.com>
2022-07-02 12:15:16 -0300

    firebase afterclass ejemplo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,7 @@
-import styled from "styled-components";
+function Button({ text, color = "blue", className = "" }) {
+  let styles = `bg-${color}-500 rounded-lg font-bold text-white mx-auto ${className}`;
 
-// Creamos un <button> con styled-components
-export const MyButton = styled.button`
-  align-items: center;
-  color: white;
-  display: flex;
-  justify-content: center;
-  padding: 10px 20px;
-  height: 40px;
-  margin: 20px auto;
-  background-color: ${(props) => props.color};
-`;
-/*  */
-function Button({ color }) {
-  return <MyButton color={color}>Click me!</MyButton>;
+  return <button className={styles}>{text}</button>;
 }
 
 export default Button;
--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-import { getFirestore, getDocs, getDoc, doc, collection } from "firebase/firestore"
+import { getFirestore, getDocs, getDoc, query, where, doc, collection } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_oWFcHcpEHX9HYDplnfuRi48uIKeataQ",
@@ -12,17 +12,21 @@ const firebaseConfig = {
   appId: "1:611775867241:web:f3ca52aa39e6ea6b9575f8"
 };
 
+//1. inicializamos la app de Firebase
 const appFirebase = initializeApp(firebaseConfig);
+
+//2. inicializamos la instancia a la base de datos de Firestore
 const appFirestore = getFirestore(appFirebase);
 
-export function testDatabase(){
-    console.log(appFirestore)
-}
-
+//3. obtenemos todos los items
 export async function traerProductos(){
+    // A. Instanciamos una colección (appfirestore, nombre-colecccion)
     const plantCollection = collection(appFirestore, "plants");
-    const plantsSnapshot = await getDocs(plantCollection);
 
+    //B. Obtenemos los documentos con getDocs(referncia-de-la-coleccion)
+    const plantsSnapshot = await getDocs(plantCollection)
+
+    //C. Del snapshot, mapeamos los documentos
     let respuesta = plantsSnapshot.docs.map( doc => { 
         return {
             ...doc.data(),
@@ -31,6 +35,25 @@ export async function traerProductos(){
     })
 
    return respuesta;
+}
+
+export async function traerProductosDeCategoria(idcategory){
+    /* query "where" */
+    
+    const plantCollection = collection(appFirestore, "plants");
+
+    const q = query(plantCollection, where("category", "==", idcategory));
+
+    const plantsSnapshot = await getDocs(q);
+
+    let respuesta = plantsSnapshot.docs.map( doc => { 
+        return {
+            ...doc.data(),
+            id: doc.id
+        }
+    })
+
+    return respuesta
 }
 
 export async function traerUnProducto(itemId){
